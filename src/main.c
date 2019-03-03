462faa045108cbac21da276c0a67287d8b4fa54b
Michal Krombholz <krombholz@gmail.com>
2019-03-03 13:13:31 -0800

    LED bink on timer interrupt and key interrupt toggles the frequency

--- src/main.c
+++ src/main.c
@@ -25,33 +25,54 @@
  */
 int main(void)
 {
-    WDTCTL  = WDTPW + WDTHOLD; 	// Stop WDT 0x5a80
-	BCSCTL1 = CALBC1_1MHZ;      // original MOV.B   #0xcc,  &0x0057  ; BCSCTL1
-  	DCOCTL  = CALDCO_1MHZ;      // original MOV.B   #0x68,  &0x0056  ; DCOCTL
-  	BCSCTL3 |= 0x20;            // XT2S=0 LFXT1Sx=VLOCLK
-  	
+	WDTCTL = WDTPW + WDTHOLD; // Stop WDT 0x5a80
+	BCSCTL1 = CALBC1_1MHZ;	  // original MOV.B   #0xcc,  &0x0057  ; BCSCTL1
+	DCOCTL = CALDCO_1MHZ;	  // original MOV.B   #0x68,  &0x0056  ; DCOCTL
+	BCSCTL3 |= 0x20;		  // XT2S=0 LFXT1Sx=VLOCLK
 
-	P1DIR = P_LED|P_CS|P_SCLK|P_MOSI; // P1DIR 0x65=01100101 --> 6,5,2,0 out (P1.0 led; P1.2=CS; P1.5=SCLK, P1.6=SI)
-	P1SEL = 0; // select special io functions - normal io
+	// Port 1 setup
+	P1DIR = P_LED | P_CS | P_SCLK | P_MOSI; // P1DIR 0x65=01100101 --> 6,5,2,0 out (P1.0 led; P1.2=CS; P1.5=SCLK, P1.6=SI)
+	P1SEL = 0;								// select special io functions --> normal io
 
-	P2DIR = 0;    // P2 is all in
+	// Port 2 setup
+	P2DIR = 0;	  // P2 is all in
 	P2REN = 0xff; // resistors enable - all
 	P2OUT = 0xff; // pull up - all
-	P2SEL = 0;    // select special io functions - normal io
-	
+	P2SEL = 0;	  // select special io functions --> normal io
+
 	// enable interrupt for key switch
-	P2IFG = 0; // clear flags
-	P2IE = P_KEY; // enable
+	P2IFG = 0;	   // clear flags register
+	P2IE = P_KEY;  // enable
 	P2IES = P_KEY; // edge
 
-	while(1)
+	// setup timer A0
+	CCTL0 = CCIE;					// CCR0 interrupt enabled
+	TACTL = TASSEL_2 + ID_3 + MC_1; // TASSEL_2 --> SMCLK (~1MHz), MC_1--> up, ID_3 --> SMCLK/8
+	CCR0 = 50000;					// (SMCLK/8)/50000 = 2.5 Hz
+
+	// Enter Low Power Mode = LPM0 w/ interrupt
+	_BIS_SR(CPUOFF + GIE);
+
+	// never here - just hang, no exit
+	while (1)
+		;
+}
+
+// Timer A0 interrupt service routine
+#pragma vector = TIMERA0_VECTOR
+__interrupt void Timer_A(void)
+{
+	P1OUT ^= P_LED; // toggle LED for now
+}
+
+// Port 1 interrupt service routine
+#pragma vector = PORT2_VECTOR
+__interrupt void Port_2(void)
+{
+	// TODO - there is no debouncing of the switch
+	if( P2IFG & P_KEY )
 	{
-		LED(1);
-		DELAYLOOP(0xFFFF)
-		LED(0);
-		DELAYLOOP(0xFFFF)
+		CCR0 = CCR0 > 10000 ? 10000 : 50000; // toggle between two blink speeds
 	}
- 
-	// hang, not exit
-    while(1);
+	P2IFG = 0; // clear all flags
 }
